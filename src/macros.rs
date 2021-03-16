971b7801c31788eca6c011b14e6812dad330b9db
Teoh Han Hui <teohhanhui@gmail.com>
2021-03-17 03:51:36 +0800

    Fix macro hygiene

--- src/macros.rs
+++ src/macros.rs
@@ -2,14 +2,14 @@
 macro_rules! pipe {
     ($a:expr, $b:expr) => ($b($a));
     ($a:expr, $b:expr, $($rest:expr),*) => {
-        pipe!($b($a), $($rest),*)
+        $crate::pipe!($b($a), $($rest),*)
     };
 }
 
 #[macro_export]
 macro_rules! merge {
-    ($a:expr, $b:expr) => (merge($a, $b));
+    ($a:expr, $b:expr) => ($crate::operators::merge($a, $b));
     ($a:expr, $b:expr, $($rest:expr),*) => {
-        merge!(merge($a, $b), $($rest),*)
+        $crate::merge!($crate::operators::merge($a, $b), $($rest),*)
     };
 }
